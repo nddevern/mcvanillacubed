757a8e16e164acc1f54fd6261b75fd7c70a1edd4
Noah Deverney <nodever2@gmail.com>
2024-11-14 07:36:22 -0500

    Add repository to server and server configs

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/nodever2/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/java/java-runtime-gamma/bin/java  -Xmx4G -jar fabric-server-mc.1.20.1-loader.0.16.7-launcher.1.0.1.jar nogui^C
