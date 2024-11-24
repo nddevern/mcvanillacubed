b224639df68ed0f01d504a963efb58bebf089958
Noah Deverney <nodever2@gmail.com>
2024-11-23 22:10:01 -0500

    Add ability to create bronze blend out of copper and tin ingots

--- kubejs/server_scripts/server_scripts.js
+++ kubejs/server_scripts/server_scripts.js
@@ -41,6 +41,10 @@ ServerEvents.recipes(event => {
         result: 'beersteel:steel_ingot',
         experience: 0.3
     });
+
+    event.shapeless(Item.of('bronze:bronze_blend'), ['3x minecraft:copper_ingot', 'bronze:tin_ingot'])
+    event.shapeless(Item.of('bronze:bronze_blend'), ['3x minecraft:raw_copper',   'bronze:tin_ingot'])
+    event.shapeless(Item.of('bronze:bronze_blend'), ['3x minecraft:copper_ingot', 'bronze:raw_tin'])
 });
 
 
