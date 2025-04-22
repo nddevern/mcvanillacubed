664b606c18783a1f4012801f8ebb623a6cc1ab8b
Noah Deverney <nddevern@mtu.edu>
2025-04-21 21:49:04 -0400

    Update run.sh and run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-java -Xmx16G -jar fabric-server-mc.1.20.1-loader.0.16.7-launcher.1.0.1.jar nogui
+java -Xmx16G -jar fabric-server-mc.1.20.1-loader.0.16.11-launcher.1.0.3.jar nogui
 @pause
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/nodever2/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/java/java-runtime-gamma/bin/java  -Xmx12G -jar fabric-server-mc.1.20.1-loader.0.16.7-launcher.1.0.1.jar nogui^C
+/home/nodever2/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/java/java-runtime-gamma/bin/java  -Xmx12G -jar fabric-server-mc.1.20.1-loader.0.16.11-launcher.1.0.3.jar nogui^C
